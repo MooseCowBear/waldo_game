7822ffd82eaad791785cfaf1f331fcb039845938
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-08-30 12:39:57 -0400

    add zooming

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -3,12 +3,15 @@ import { found } from "../helpers/game_helpers";
 
 export default Game = () => {
   const [level, setLevel] = useState(0); // will be used when we start fetching...
+  const [zoomLevel, setZoomLevel] = useState(0);
+
+  // TODO: need a timer
 
   // temp, this is for level 0 image
   const normalizedWinningX = 0.138671875;
   const normalizedWinningY = 0.1605839416;
 
-  const clickHandler = (e) => {
+  const imageClickHandler = (e) => {
     console.log("image div was clicked.");
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
@@ -30,18 +33,44 @@ export default Game = () => {
     );
 
     console.log("found?", charFound);
+
+    // TODO: if the click is successful need to stop the timer and then load the next level
   };
+
+  const zoomInHandler = () => {
+    if (zoomLevel < 2) {
+      setZoomLevel((zoomLevel) => zoomLevel + 1);
+    }
+  };
+
+  const zoomOutHandler = () => {
+    // might not want to go out as far as -1
+    if (zoomLevel > 0) {
+      setZoomLevel((zoomLevel) => zoomLevel - 1);
+    }
+  }
 
   return (
     <div className="game wrapper">
       <div className="header">
-        <p>zoom</p>
+        <div className="button-wrapper">
+          <button onClick={zoomInHandler}>zoom in</button>
+          <button onClick={zoomOutHandler}>zoom out</button>
+        </div>
         <img className="thumbnail" src="images/thumb_level_0.jpeg" alt="" />
         <p>info</p>
       </div>
       <div className="outer-image-wrapper">
-        <div className="image-wrapper" onClick={clickHandler}>
-          <img className="image" src="images/level_0.jpeg" alt="" />
+        <div
+          className={`image-wrapper zoom-${zoomLevel}`}
+          onClick={imageClickHandler}
+        >
+          <img
+            className={`image zoom-${zoomLevel}`}
+            id="game-image"
+            src="images/level_0.jpeg"
+            alt=""
+          />
         </div>
         <div className="bounding-box"></div>
       </div>
